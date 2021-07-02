36aa80299eeee995f018123a9a81a69ad2a7134c
David Zhivkov <davidzhivkov@gmail.com>
2021-07-02 17:31:16 +0100

    modify scripts

--- choco-install-wsl2.cmd
+++ choco-install-wsl2.cmd
@@ -1,7 +1,10 @@
 REM Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dzhivkov/public-scripts/main/choco-install-wsl2.cmd" -OutFile "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup-wsl.cmd"
-PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-runas /user:administrator choco install -y wsl2 >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dzhivkov/public-scripts/main/choco-install-wsl-ubuntu-2004.cmd" -OutFile "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup-wsl-ubuntu.cmd" >> "%TEMP%\StartupLog.txt" 2>&1
+REM PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
+REM runas /user:administrator choco install -y wsl2 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell -Command "Set-ExecutionPolicy Unrestricted"
+runas /user:administrator choco install -y wsl2
+pause
+REM PowerShell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dzhivkov/public-scripts/main/choco-install-wsl-ubuntu-2004.cmd" -OutFile "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup-wsl-ubuntu.cmd" >> "%TEMP%\StartupLog.txt" 2>&1
 
-shutdown -r -f -t 30
-del "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup-wsl.cmd"
+REM shutdown -r -f -t 05
+REM del "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup-wsl.cmd"
