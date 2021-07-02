758c40701449e4e0467050da7e5ae9f401055508
David Zhivkov <davidzhivkov@gmail.com>
2021-07-02 16:42:43 +0100

    modify scripts

--- choco-install-wsl-ubuntu-2004.cmd
+++ choco-install-wsl-ubuntu-2004.cmd
@@ -1,5 +1,5 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell choco install -y wsl-ubuntu-2004 >> "%TEMP%\StartupLog.txt" 2>&1
+PowerShell runas /user:administrator choco install -y wsl-ubuntu-2004 >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell Ubuntu2004.exe install --root
 PowerShell wsl.exe sudo apt-get update
 PowerShell wsl.exe sudo apt-get install -y ansible
--- choco-install-wsl2.cmd
+++ choco-install-wsl2.cmd
@@ -1,6 +1,6 @@
 REM Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dzhivkov/public-scripts/main/choco-install-wsl2.cmd" -OutFile "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup-wsl.cmd"
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
-PowerShell choco install -y wsl2 >> "%TEMP%\StartupLog.txt" 2>&1
+runas /user:administrator choco install -y wsl2 >> "%TEMP%\StartupLog.txt" 2>&1
 PowerShell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dzhivkov/public-scripts/main/choco-install-wsl-ubuntu-2004.cmd" -OutFile "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\startup-wsl-ubuntu.cmd" >> "%TEMP%\StartupLog.txt" 2>&1
 
 shutdown -r -f -t 30
