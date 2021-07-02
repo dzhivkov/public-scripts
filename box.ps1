d02da5baa6b84b90527918b090180bd98c5e7ef7
David Zhivkov <davidzhivkov@gmail.com>
2021-07-02 22:07:49 +0100

    modify scripts

--- box.ps1
+++ box.ps1
@@ -1,6 +1,1 @@
-choco install -y wsl2
-choco install -y wsl-ubuntu-2004
-Ubuntu2004.exe install --root
-wsl.exe sudo apt-get update
-wsl.exe sudo apt-get install -y ansible
-
+choco install -y winmerge
