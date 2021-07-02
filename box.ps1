37fb1427da6a07fc937bb405be1b51a87db157cc
David Zhivkov <davidzhivkov@gmail.com>
2021-07-02 23:01:54 +0100

    modify scripts

--- box.ps1
+++ box.ps1
@@ -1,1 +1,3 @@
 choco install -y winmerge
+choco install -y vlc
+
---
+++ run-box.ps1
@@ -0,0 +1,2 @@
+BoxstarterShell
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/dzhivkov/public-scripts/main/box.ps1
